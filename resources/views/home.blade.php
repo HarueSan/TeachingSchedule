36a47de47623335e2629f935857b4ba9b3c20676
peeteerapong <37838407+peeteerapong@users.noreply.github.com>
2020-09-28 14:28:45 +0700

    Update Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,48 @@
-<h1> welcome to SE project <h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CS CMU Teaching Schedule</title>
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Ubuntu" />
+    <style>
+    html,body {
+        margin:0;
+        padding:0;
+    }
+    font,h1,h2,h3 {
+        font-family: Ubuntu;
+        font-style:normal;
+        font-variant: normal; 
+    }
+    #logo {
+        width: calc(100% - 96%);
+    }
+    #header{
+        display: flex;
+        flex-direction: row;
+        background-color: black;
+    }
+    #header > font {
+        color: white;
+        margin: 0.4% 0%;
+        font-size:x-large;
+    }
+    #content{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: calc(100% - 97%);
+    }
+    </style>
+</head>
+<body>
+    <div id='header'>
+    <img id='logo' src='Logo.png'>
+    <font>Teaching Schedule</font>
+    </div>
+    <div id='content'>
+    <h2>CS CMU Teaching Schedule</h2>
+
+</body>
+</html>
