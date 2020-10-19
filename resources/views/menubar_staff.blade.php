055752451990f356c2974a4f9803ad814545782a
HarueSan <oonnaka2012@gmail.com>
2020-10-19 23:04:07 +0700

    update menubar
    linkหน้าทั้งหมด

--- resources/views/menubar_staff.blade.php
+++ resources/views/menubar_staff.blade.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col" style="height: 100px;">
             <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="/staff">
                     <img src="https://sv1.picz.in.th/images/2020/10/17/bSYkZR.png" weight="40px" height="40px" alt="logo">
                     Teaching Schedule
                 </a>
@@ -13,26 +13,29 @@
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="#"><i class="fa fa-home"></i> Home
-                                <span class="sr-only">(current)</span>
+                            <a class="nav-link" href="/staff">
+                            <i class="fa fa-home"></i> Home
+                            <span class="sr-only">(current)</span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fas fa-plus-circle"></i>Add
+                            <a class="nav-link" href="/add">
+                            <i class="fas fa-plus-circle"></i> Add
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fas fa-pen-nib"></i> Manage
+                            <a class="nav-link" href="/manage">
+                            <i class="fas fa-pen-nib"></i> Manage
                             </a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-calendar-alt"></i> Schedule
+                            <i class="fas fa-calendar-alt"></i> Schedule
                             </a>
                             <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-                                <a class="dropdown-item" href="#">Choose from teachers</a>
+                                <a class="dropdown-item" href="/showbyteacher">Choose from teachers</a>
                                 <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Choose from date and time</a>
+                                <a class="dropdown-item" href="/showbydate">Choose from date and time</a>
                             </div>
                         </li>
                     </ul>
--- resources/views/menubar_teacher.blade.php
+++ resources/views/menubar_teacher.blade.php
@@ -1,31 +1,37 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">
-        <img src="https://sv1.picz.in.th/images/2020/10/17/bSYkZR.png" weight="40px" height="40px" alt="logo">
-        Teaching Schedule
-    </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">
-                    <i class="fa fa-home"></i> Home
-                    <span class="sr-only">(current)</span>
+<div class="container" style="max-width: 100%; margin: 0;">
+    <div class="row">
+        <div class="col" style="height: 100px;">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="/teacher">
+                    <img src="https://sv1.picz.in.th/images/2020/10/17/bSYkZR.png" weight="40px" height="40px" alt="logo">
+                    Teaching Schedule
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                <i class="fas fa-edit"></i> Select</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                <i class="fas fa-calendar-alt"></i> Schedule</a>
-            </li>
-        </ul>
-        <span class="navbar-text">
-            Username
-        </span>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/teacher">
+                            <i class="fa fa-home"></i> Home
+                            <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/select">
+                            <i class="fas fa-edit"></i> Select</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/show">
+                        <i class="fas fa-calendar-alt"></i> Schedule</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                    Username
+                    </span>
+                </div>
+            </nav>
+        </div>
     </div>
-</nav>
+</div>
