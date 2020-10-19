0e10aba6dfbdaf283c708a16d70ff5605fa76582
HarueSan <oonnaka2012@gmail.com>
2020-10-19 22:51:08 +0700

    update menubar
    อันเดิมมันกินพื้นที่หน้าจอ อันนี้ไม่กินแล้ว

--- resources/views/staff/manage_staff.blade.php
+++ resources/views/staff/manage_staff.blade.php
@@ -20,5 +20,43 @@
                     @include('menubar_staff')
             </div>
         </div>
-    </body>
+        <div>
+        <center>
+        <body><h1 style="margin-bottom: 30px;">ตารางสอน</h1>
+        </body>
+        </center>
+
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm" style="margin-left: 50px;"> รหัสวิชา </span>
+            </div>
+        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+            <select name="dayType1" id="" class="form-control" >
+                <option selected>วัน</option>
+                    <option value="1234">จันทร์</option>
+                    <option value="3456">อังคาร</option>
+                    <option value="1234">พุธ</option>
+                    <option value="3456">พฤหัสบดี</option>
+                    <option value="1234">ศุกร์</option>
+            </select>
+            <select name="timeType1" id="" class="form-control" >
+                <option selected>ช่วงเวลา</option>
+                    <option value="1234">08:00 - 09:30</option>
+                    <option value="3456">09:30 - 11:00</option>
+                    <option value="1234">11:00 - 12:30</option>
+                    <option value="3456">13:00 - 14:30</option>
+                    <option value="1234">14:30 - 16:00</option>
+            </select><select name="dayType1" id="" class="form-control" >
+                <option selected>ห้อง</option>
+                    <option value="1234">Lec CSB100</option>
+                    <option value="3456">Lec CSB201</option>
+                    <option value="1234">Lec CSB207</option>
+                    <option value="3456">Lec CSB208</option>
+                    <option value="1234">Lec CSB209</option>
+                    <option value="1234">Lec CSB210</option>
+                    <option value="3456">Lab CSB301</option>
+                    <option value="1234">Lab CSB307</option>
+                    <option value="3456">Lab CSB308</option>
+            </select>
+    <button type="button" class="btn btn-warning" style="margin-right: 50px;"> เพิ่ม </button>
 </html>
