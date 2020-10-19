ada6aa49ec24400bee67f014083aee72aee33344
HarueSan <oonnaka2012@gmail.com>
2020-10-19 12:05:52 +0700

    menubar sort
    จัดระเบียนให้เป็นหมวดหมู่ ตั้งชื่อในweb.phpทั้งหมด เรียกดึงmenubarไปใช้ได้ มี2แบบ เป็นของอาจารย์กับธุรการ

---
+++ resources/views/menubar_staff.blade.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="https://sv1.picz.in.th/images/2020/10/17/bSYkZR.png" weight="40px" height="40px" alt="logo">
+        Teaching Schedule
+    </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    <i class="fa fa-home"></i> Home
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <i class="fas fa-plus-circle"></i> Add</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                <i class="fas fa-pen-nib"></i> Manage</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-calendar-alt"></i> Schedule
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                    <a class="dropdown-item" href="#">Choose from teachers</a>
+                    <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Choose from date and time</a>
+                </div>
+            </li>
+        </ul>
+        <span class="navbar-text">
+            Username
+        </span>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,41 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('index');
-});
-
+/*---------- All ----------*/
 Route::get('/login', function(){
     return view('login');
 });
 
+/*---------- Staff ----------*/
+Route::get('/staff', function () {
+    return view('staff/index');
+});
+
 Route::get('/add', function(){
-    return view('add');
+    return view('staff/add');
+});
+
+Route::get('/manage', function(){
+    return view('staff/manage_staff');
+});
+
+Route::get('/showbyteacher', function(){
+    return view('staff/show_staff_byteacher');
+});
+
+Route::get('/showbydate', function(){
+    return view('staff/show_staff_bydate');
+});
+
+/*---------- Teacher ----------*/
+Route::get('/teacher', function(){
+    return view('teacher/index_teacher');
 });
 
 Route::get('/select', function(){
-    return view('select');
+    return view('teacher/select');
+});
+
+Route::get('/show', function(){
+    return view('teacher/show_teacher');
 });
