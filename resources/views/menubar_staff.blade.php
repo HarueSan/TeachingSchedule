2c03351d9a469c00a68e4bb15388f210a96897c3
HarueSan <oonnaka2012@gmail.com>
2020-10-23 17:37:39 +0700

    update menubar
    ทำเมนูบาร์ใหม่แล้ว แต่ติดตรงบางหน้า ทำไมช่องมันใหญ่ก็ไม่รู้ แต่หน้าพวกตารางสอนไม่มีปัญหา

--- resources/views/menubar_staff.blade.php
+++ resources/views/menubar_staff.blade.php
@@ -1,37 +1,35 @@
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <div class="container" style="max-width: 100%; margin: 0;">
     <div class="row">
         <div class="col" style="height: 100px;">
-            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <nav class="navbar fixed-top navbar navbar-dark dark-blue lighten-4 bg-dark">
                 <a class="navbar-brand" href="/staff">
-                    <img src="https://sv1.picz.in.th/images/2020/10/17/bSYkZR.png" weight="40px" height="40px" alt="logo">
+                <img src="https://sv1.picz.in.th/images/2020/10/17/bSYkZR.png" weight="40px" height="40px" alt="logo">
                     Teaching Schedule
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
+                <span class="navbar-text" style="margin-left: 70%;">
+                        Username
+                </span>
+                <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+                    aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
+                    class="fas fa-bars fa-1x"></i></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent1">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="/staff">
-                            <i class="fa fa-home"></i> Home
-                            <span class="sr-only">(current)</span>
-                            </a>
+                            <a class="nav-link" style="margin-top: 10px; margin-bottom: 10px; margin-left: 10px" href="/staff">
+                            Home<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/add">
-                            <i class="fas fa-plus-circle"></i> Add
-                            </a>
+                            <a class="nav-link" style="margin-top: 10px; margin-bottom: 10px; margin-left: 10px" href="/add">
+                            Add</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/manage">
-                            <i class="fas fa-pen-nib"></i> Manage
-                            </a>
+                            <a class="nav-link" style="margin-top: 10px; margin-bottom: 10px; margin-left: 10px" href="/manage">
+                            Manage</a>
+                        </li>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-calendar-alt"></i> Schedule
-                            </a>
+                            <a class="nav-link dropdown-toggle" style="margin-top: 10px; margin-bottom: 10px; margin-left: 10px" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            </i> Schedule</a>
                             <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
                                 <a class="dropdown-item" href="/showbyteacher">Choose from teachers</a>
                                 <div class="dropdown-divider"></div>
@@ -39,9 +37,6 @@
                             </div>
                         </li>
                     </ul>
-                    <span class="navbar-text">
-                        Username
-                    </span>
                 </div>
             </nav>
         </div>
